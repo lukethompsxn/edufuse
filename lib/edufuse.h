7e7a33a528ab24a0316ebffdcac549f530895adf
lukethompson <lukethompsxn@gmail.com>
2019-06-14 20:16:47 +1200

    clean up header file

--- lib/edufuse.h
+++ lib/edufuse.h
@@ -3,7 +3,7 @@
 #ifndef EDUFUSE_EDUFUSE_H
 #define EDUFUSE_EDUFUSE_H
 
-    // feel like we shouldn't be defining 'struct' in prefix??
-    extern int edufuse_register(int argc, char *argv[], struct fuse_operations *, int size);
+
+extern int edufuse_register(int argc, char *argv[], struct fuse_operations *, int size);
 
 #endif //EDUFUSE_EDUFUSE_H
