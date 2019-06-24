9b2300d8fe3328b0b227d15f3505b39f6adceb8d
lukethompson <lukethompsxn@gmail.com>
2019-06-24 12:22:04 +1200

    gui - fix unterminated statements

--- gui/preload.js
+++ gui/preload.js
@@ -4,4 +4,4 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const versionType of ['chrome', 'electron', 'node']) {
     document.getElementById(`${versionType}-version`).innerText = process.versions[versionType]
   }
-})
+});
