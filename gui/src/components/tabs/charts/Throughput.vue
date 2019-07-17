8db8cb44cf0a20925711434d9cfdb2fd9a7d229d
Joel <joel.clarke@live.com>
2019-07-18 03:30:15 +1200

    gui - implemented basic chart with fs data.

--- gui/src/components/tabs/charts/Throughput.vue
+++ gui/src/components/tabs/charts/Throughput.vue
@@ -1,11 +1,20 @@
 <template>
-    <div>
-        <highcharts :options="chartOptions"></highcharts>
-    </div>
+    <b-container class="bv-example-row" style="height: 100%">
+        <b-row style="height: 100%;">
+            <div>
+                <highcharts :options="chartOptions" :updateArgs="updateArgs" ref="chart"></highcharts>
+            </div>
+            <div v-for="index in 4" :key="index">
+                <p>{{index}}</p>
+                <input v-model.number="points[index-1]" type="number">
+            </div>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
     import {Chart} from 'highcharts-vue'
+    import {messageBus} from '../../../main.js';
 
     export default {
         name: "Throughput",
@@ -14,16 +23,60 @@
         },
         data() {
             return {
+                title: '',
+                points: [0, 0, 0, 0],
+                chartType: 'Column',
+                seriesColor: '#6fcd98',
+                colorInputIsSupported: null,
+                animationDuration: 1000,
+                updateArgs: [true, true, {duration: 1000}],
                 chartOptions: {
-                    series: [{
-                        data: [1,2,3] // sample data
-                    }],
-                    credits: false,
                     chart: {
-                        backgroundColor: "transparent"
-                    }
+                        type: 'column',
+                        backgroundColor: 'transparent'
+                    },
+                    title: {
+                        text: 'Test Chart'
+                    },
+                    credits: false,
+                    xAxis: {
+                        categories: ['getattr', 'readdir', 'open', 'read']
+                    },
+                    series: [{
+                        data: [0, 0, 0, 0],
+                        color: '#6fcd98',
+                    }]
                 }
             }
+        },
+        methods: {
+            updateValues(msg) {
+                if (msg !== undefined && msg === 'getattr') this.points[0]++;
+                else if (msg !== undefined && msg === 'readdir') this.points[1]++;
+                else if (msg !== undefined && msg === 'open') this.points[2]++;
+                else if (msg !== undefined && msg === 'read') this.points[3]++;
+
+                //this.points.push();
+                this.updateSeries(this.points);
+
+                console.log(msg);
+            },
+            updateSeries(newValue) {
+                console.log(newValue);
+                this.chartOptions.series[0].data = newValue;
+                this.points.push();
+            },
+        },
+        created: function () {
+            messageBus.$on('LOG', (json) => {
+                this.updateValues(json.syscall);
+
+            });
+        },
+        watch: {
+            points (newValue) {
+                this.chartOptions.series[0].data = newValue;
+            },
         }
     }
 </script>
