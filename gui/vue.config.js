44c7404d4231f2b63e151a7e9fb3b1f4c9d59a9b
lukethompson <lukethompsxn@gmail.com>
2019-08-21 19:10:18 +1200

    build - try building electron app to native

--- gui/vue.config.js
+++ gui/vue.config.js
@@ -1,3 +1,39 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? `${process.cwd()}/dist/` : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? `${process.cwd()}/dist/` : '/',
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        productName: "eduFUSE",
+        appId: "com.github.lukethompsxn.edufuse",
+        directories: {
+          output: "build"
+        },
+        // dmg: {
+        //   contents: [
+        //     {
+        //       x: 855,
+        //       y: 600,
+        //       type: "link",
+        //       path: "/Applications"
+        //     },
+        //     {
+        //       x: 130,
+        //       y: 150,
+        //       type: "file"
+        //     }
+        //   ]
+        // },
+        mac: {
+          target: "pkg",
+          icon: "build/icons/icon.icns"
+        },
+        win: {
+          icon: "build/icons/icon.ico"
+        },
+        linux: {
+          icon: "build/icons"
+        }
+      },
+      }
+    }
 };
